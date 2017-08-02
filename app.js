92578d6beefe4e9ecef64dcc6a7ce3bd71e515ee
Chase Hemerda <chasehemerda@icloud.com>
2017-08-02 14:04:37 -0600

    express setup

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const cors = require('cors');
+
+var index = require('./routes/index');
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // render the error page
+  res.status(err.status || res.statusCode || 500);
+  res.json({message: err.message, error:req.app.get('env') === 'development' ? err : {} });
+
+module.exports = app;
