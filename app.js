557744be4853a039dc3c458b640c3de8ee1663d6
WILL MEIER <willmeierart@gmail.com>
2017-08-05 11:16:34 -0600

    cors

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var http = require('http');
+ const express = require('express');
+ const path = require('path');
+ const favicon = require('serve-favicon');
+ const cookieParser = require('cookie-parser');
+ const bodyParser = require('body-parser');
+ const http = require('http');
+ const cors = require('cors')
 
-var index = require('./routes/index');
-var sockets = require('./sockets');
+ const index = require('./routes/index');
+ const sockets = require('./sockets');
 
-var app = express();
+ const app = express();
 
-var server = http.createServer(app);
-var io = require('socket.io')(server);
+ const server = http.createServer(app);
+ const io = require('socket.io')(server);
 
 sockets(io);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,20 +24,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
