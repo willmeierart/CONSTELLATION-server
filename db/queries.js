5c9959e26f7ab6eb0041063948cd8b23eef91f04
Stephen Warton <stephen.warton@gmail.com>
2017-08-08 13:55:58 -0600

    added delete route

--- db/queries.js
+++ db/queries.js
@@ -3,12 +3,18 @@ const knex = require('./knex');
 
 
 module.exports = {
+
   getAll(){
-    return knex('pictures')
+    return knex('pictures');
   },
+
   create(picture){
-  const pictureNew = JSON.stringify(picture)
-  // console.log(pictureNew)
-  return knex('pictures').insert({data:pictureNew}, '*');
-},
-}
+    const pictureNew = JSON.stringify(picture);
+    // console.log(pictureNew)
+    return knex('pictures').insert({data:pictureNew}, '*');
+  },
+
+  deletePictureById(id){
+    return knex('pictures').where('id', id);
+  }
+};
